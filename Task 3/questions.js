a0f4eed2516ec8072e4ad1226995b4e39753e77d
Sajiree-2005 <sajireedamle@gmail.com>
2026-03-20 21:40:24 +0530

    Add Task 3: Quiz App with Score

---
+++ Task 3/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Hyper Tool Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "A. Cascading Style Sheets",
+    options: [
+      "A. Cascading Style Sheets",
+      "B. Cute Style Sheets",
+      "C. Computer Style Sheets",
+      "D. Colorful Style Sheets",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "A. Hypertext Preprocessor",
+    options: [
+      "A. Hypertext Preprocessor",
+      "B. Hometext Programming",
+      "C. Hypertext Preprogramming",
+      "D. Programming Hypertext Preprocessor",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "D. Structured Query Language",
+    options: [
+      "A. Strength Query Language",
+      "B. Stylesheet Query Language",
+      "C. Statement Question Language",
+      "D. Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "D. Extensible Markup Language",
+    options: [
+      "A. External Markup Language",
+      "B. Extended Markup Language",
+      "C. Extension Markup Language",
+      "D. Extensible Markup Language",
+    ],
+  },
+];
