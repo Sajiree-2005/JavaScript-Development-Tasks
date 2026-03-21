e5ca9f9d55b5c1832195202a3764033c8a439ea6
Sajiree-2005 <sajireedamle@gmail.com>
2026-03-21 13:38:52 +0530

    Add Task 4: Weather App using API

---
+++ Task 4/script.js
@@ -0,0 +1,38 @@
+const apiKey = "62ace67a6b6f8d359ca7dde6ad11d592";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    // console.log(data);  From here you can see the parameters that an API will return
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/cloudy.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/sun.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/snowy.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/wind.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
